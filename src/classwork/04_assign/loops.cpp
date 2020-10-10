fb5e755d202302b85d5a968e1fb160f2b32a8c86
JosephBizra <j.bizra@gmail.com>
2020-10-09 23:41:30 -0500

    Turned in homework

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,21 @@
 //cpp
+#include<iostream>
+#include "loops.h"
+using namespace std;
+
+int factorial(int num)
+{
+    // keep one separate variable to multiply. Keep it at 1 so 0 doesn't get multiplied.
+    int fact = 1;
+
+    // loop through all integers between 0 and num.
+    for (int i = 1; i <= num; i++)
+    {
+        cout<<"\ni = "<<i;
+        // keep multiplying fact by i
+        fact = fact * i;
+        cout<<"  newNum = "<<fact;
+    }
+    
+    return fact;
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,9 @@
 //write includes statements
-
+#include<iostream>
+#include "loops.h"
 //write using statements for cin and cout
 
+using namespace std;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +12,35 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	// placeholder number
+	int num;
+	
+	// user input
+	cout<<"Enter a number between 1 and 20 or type 0 to exit: ";
+	cin>>num;
+
+	// verify user doesn't want to exit
+	while (num != 0)
+	{
+		// verify input is between 1 and 20
+		while (num < 1 || num > 20)
+		{
+			cout<<"Please give a number between 1 and 20 ";
+			cin>>num;
+		}
+		
+		// call function
+		int value = factorial(num);
+
+		// display results
+		cout<<"\nYour factorial is: "<<value;
+
+		// prompt user again
+		cout<<"\nEnter a number between 1 and 20 or type 0 to exit: ";
+		cin>>num;
+	}
+	
+
 
 	return 0;
 }
